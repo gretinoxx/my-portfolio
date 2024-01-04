12bc7a5f74fe5034d7d99cb86fbc0a478468d950
emmariescurrena <emmariescurrena>
2024-01-04 17:58:04 -0300

    Corrected files

--- script.js
+++ script.js
@@ -22,18 +22,20 @@ let currentThemeSetting = calculateSettingAsThemeString({
 });
 
 const button = document.querySelector("[data-theme-toggle]");
+const buttonImg = document.getElementById("buttonImg");
 
 button.addEventListener("click", () => {
   const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
 
-  // update the button text
+  // update the img and the aria-label
   const newCta =
     newTheme === "dark" ? "Change to light theme" : "Change to dark theme";
-  button.innerText = newCta;
-
-  // use an aria-label if you are omitting text on the button
-  // and using sun/moon icons, for example
-  // button.setAttribute("aria-label", newCta);
+  src =
+    newTheme === "dark"
+      ? "./img/day-and-night-icon-dark.svg"
+      : "./img/day-and-night-icon-light.svg";
+  buttonImg.setAttribute("src", src);
+  button.setAttribute("aria-label", newCta);
 
   // update theme attribute on HTML to switch theme in CSS
   document.querySelector("html").setAttribute("data-theme", newTheme);
