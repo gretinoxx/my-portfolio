567498985c32627be5b20640a6843d9959024d51
emmariescurrena <125983228+emmariescurrena@users.noreply.github.com>
2024-06-09 10:01:07 -0300

    Github img changes with dark/light theme

--- script.js
+++ script.js
@@ -23,17 +23,24 @@ let currentThemeSetting = calculateSettingAsThemeString({
 
 const button = document.getElementById("button");
 const buttonImg = document.getElementById("buttonImg");
+const githubImg = document.getElementById("githubImg");
 
 function updateTheme(newTheme) {
   // update the img and the aria-label
   const newCta =
     newTheme === "dark" ? "Change to light theme" : "Change to dark theme";
-  src =
+  buttonSrc =
     newTheme === "dark"
       ? "./img/day-and-night-icon-dark.svg"
       : "./img/day-and-night-icon-light.svg";
-  buttonImg.setAttribute("src", src);
+  buttonImg.setAttribute("src", buttonSrc);
   button.setAttribute("aria-label", newCta);
+
+  githubSrc =
+    newTheme === "dark"
+      ? "./img/github-mark-white.svg"
+      : "./img/github-mark.svg";
+  githubImg.setAttribute("src", githubSrc);
 
   // update theme attribute on HTML to switch theme in CSS
   document.querySelector("html").setAttribute("data-theme", newTheme);
